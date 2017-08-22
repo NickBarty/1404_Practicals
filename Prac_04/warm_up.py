8cd4f6e5438cfeb37c8196d4dbab6bee544f6810
Nicholas Barty <nick.barty@outlook.com>
2017-08-22 13:17:59 +1000

    Added missed warm_up file from previous push

---
+++ Prac_04/warm_up.py
@@ -0,0 +1,11 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers)
+numbers[0] = "ten"
+print(numbers)
+numbers[-1] = 1
+print(numbers)
+print(numbers[2:])
+if 9 in numbers:
+    print("9 is in the list")
+else:
+    print("9 is not in the list")
