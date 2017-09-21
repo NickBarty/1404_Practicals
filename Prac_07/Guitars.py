b76978394214c15c09e344a5eac848d40c70bb84
nbart1 <nicholas.barty@my.jcu.edu.au>
2017-09-21 10:27:49 +1000

    Add new functionality for Prac_07 guitars exercise

--- Prac_07/Guitars.py
+++ Prac_07/Guitars.py
@@ -14,18 +14,20 @@ def main():
         print(guitar_to_add, "added.")
         name = input("Name: ")
 
-    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     # guitars.sort()
     print("These are my guitars:")
 
-    if guitars is not None:
-        for i, guitar in enumerate(guitars):
-            vintage_string = "(vintage)" if guitar.is_vintage() else ""
-            print("Guitar {0}: {1.name:>10} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i + 1, guitar, vintage_string))
-    else:
-        print("No guitars")
+    # if guitars is not None:
+    i = 0
+    for guitar in guitars:
+        vintage_string = "(vintage)" if guitar.is_vintage() else ""
+        print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i, guitar, vintage_string))
+        i += 1
+    # else:
+    #     print("No guitars")
 
 
 main()
