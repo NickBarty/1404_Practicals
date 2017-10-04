27aedf93d2c79d4e1333266d407bd0bc4d234d3e
nbart1 <nicholas.barty@my.jcu.edu.au>
2017-10-05 09:45:26 +1000

    Add Prac_08 extension work

--- Prac_08/unreliable_car_test.py
+++ Prac_08/unreliable_car_test.py
@@ -1,7 +1,6 @@
 from Prac_08.unreliable_car import UnreliableCar
 
 prius = UnreliableCar('prius', 100)
-prius.drive(45)
-print(prius)
-prius.drive(50)
-print(prius)
+for count in range(5):
+    prius.drive(5)
+    print(prius)
