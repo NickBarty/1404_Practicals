1f186944f857922e445f9f4bb3eec27fa92e4237
nbart1 <nicholas.barty@my.jcu.edu.au>
2017-10-19 10:27:29 +1000

    Add extra file renaming functionality

--- Prac_09/cleanup_files.py
+++ Prac_09/cleanup_files.py
@@ -2,35 +2,66 @@
 CP1404/CP5632 Practical
 File renaming and os examples
 """
+import shutil
 import os
 
-print("Current directory is", os.getcwd())
 
-# change to desired directory
-os.chdir('Lyrics/Christmas')
-# print a list of all files (test)
-# print(os.listdir('.'))
+def main():
+    """Demo file renaming with the os module."""
+    print("Current directory is", os.getcwd())
 
-# make a new directory
-# os.mkdir('temp')
+    # change to desired directory
+    os.chdir('Lyrics/Christmas')
+    # print a list of all files (test)
+    print(os.listdir('.'))
 
-# loop through each file in the (original) directory
-for filename in os.listdir('.'):
-    # ignore directories, just process files
-    if not os.path.isdir(filename):
-        new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-        print(new_name)
+    # make a new directory
+    # os.mkdir('temp')
 
-        # Option 1: rename file to new name - in place
-        os.rename(filename, new_name)
+    # loop through each file in the (original) directory
+    for filename in os.listdir('.'):
+        # for root, dirs, files in os.walk(".."):
+        #     for filename in files:
+        #         new_name = get_fixed_filename(filename)
+        #         print(new_name)
+        # ignore directories, just process files
+        if not os.path.isdir(filename):
+            new_name = get_fixed_filename(filename)
+            print(new_name)
 
-        # Option 2: move file to new place, with new name (import shutil first)
-        # shutil.move(filename, 'temp/' + new_name)
+            # NOTE: These options won't just work...
+            # they show you ways of renaming and moving files,
+            # but you need to have valid filenames. You can't make a file with
+            # a blank name, and on Windows you can't have files with the same
+            # characters but different case (e.g. a.TXT and A.txt)
+            # So, you need to get valid filenames before you can use these.
+
+            # Option 1: rename file to new name - in place
+            # os.rename(filename, new_name)
+
+            # Option 2: move file to new place, with new name
+            # shutil.move(filename, 'temp/' + new_name)
+
+            # Processing subdirectories using os.walk()
+
+            # os.chdir('..')  # .. means "up" one directory
+            # for dir_name, subdir_list, file_list in os.walk('.'):
+            #     print("In", dir_name)
+            #     print("\tcontains subdirectories:", subdir_list)
+            #     print("\tand files:", file_list)
 
 
-# Processing subdirectories using os.walk()
-os.chdir('..')
-for dir_name, subdir_list, file_list in os.walk('.'):
-    print("In", dir_name)
-    print("\tcontains subdirectories:", subdir_list)
-    print("\tand files:", file_list)
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    # First, replace the spaces and .TXT (the easy part)
+    filename = filename.replace(" ", "_").replace(".TXT", ".txt")
+    if "_" not in filename:
+        filename_letters = list(filename)
+        for index, letter in enumerate(filename_letters):
+            if index > 0 and letter.isupper() and filename_letters[index - 1].isalpha():
+                filename_letters.insert(index, "_")
+        filename = "".join(filename_letters)
+    return filename
+
+
+main()
